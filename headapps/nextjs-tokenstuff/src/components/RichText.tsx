59aad9885956facb141da01c3be77759a0389ab2
Christian <christian-hahn1@gmx.net>
2025-07-01 13:15:38 +0200

    - renaming

---
+++ headapps/nextjs-tokenstuff/src/components/RichText.tsx
@@ -0,0 +1,31 @@
+import React, { JSX } from 'react';
+import { Field, RichText } from '@sitecore-content-sdk/nextjs';
+// import RichTextWrapper from './custom/wrapper/RichTextWrapper';
+
+interface Fields {
+  Text: Field<string>;
+}
+
+export type RichTextProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+export const Default = (props: RichTextProps): JSX.Element => {
+  const text = props.fields ? (
+    // <RichTextWrapper field={props.fields.Text} doReplacement={true} />
+    <RichText field={props.fields.Text} />
+  ) : (
+    <span className="is-empty-hint">Rich text</span>
+  );
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div
+      className={`component rich-text ${props.params.styles.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="component-content">{text}</div>
+    </div>
+  );
+};
